6d8ab029f224f36ed1b2b77ef90a4d6c0a36e0bf
jaanmeld <jaanmeld@gmail.com>
2024-02-28 18:55:40 +0100

    basic footer with all dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import AppRouter from './AppRouter.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
