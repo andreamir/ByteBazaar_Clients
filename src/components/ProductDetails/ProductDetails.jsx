d7684b4a1576217265411c58ab7ffda16a1019c3
jaanmeld <jaanmeld@gmail.com>
2024-02-29 20:08:52 +0100

    design at 90%

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,47 +1,89 @@
-
+import pcUrl from '../../assets/platforms/PC.png'
+import switchUrl from '../../assets/platforms/NintendoSwitch.png'
+import play1Url from '../../assets/platforms/PlayStation1.png'
+import play4Url from '../../assets/platforms/PlayStation4.png'
+import play5Url from '../../assets/platforms/PlayStation5.png'
+import wiiUrl from '../../assets/platforms/wii.png'
+import xboxUrl from '../../assets/platforms/xbox.png'
 
 function ProductDetails(props){
    const { product } = props
-console.log(product);
    const { title, image } = product.gameTitle_id;
    const platformName = product.platform_id.name;
    const { _id, price, stock } = product
+   let overlayUrl =''
+
+   switch (platformName) {
+    case 'PC':
+      overlayUrl = pcUrl
+      break;
+    case 'XBoxSeries':
+    case 'XBoxOne':
+      overlayUrl = xboxUrl
+      break;
+    case 'PlayStation4':
+      overlayUrl = play4Url
+      break;
+    case 'PlayStation5':
+      overlayUrl = play5Url
+      break;  
+    case 'PlayStation1':
+      overlayUrl = play1Url
+      break;
+    case 'Wii':
+      overlayUrl = wiiUrl
+      break;
+    case 'NintendoSwitch':
+      overlayUrl = switchUrl
+      break;    
+    default:
+      console.log('platform name is missing');
+  }
 
   return(
     <>
-    <div className="container my-5">
-  <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+      <div className="container my-5">
+        <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+          <div className="col-lg-4 p-0 overflow-hidden shadow-lg order-lg-1">
+            <div className="lc-block" style={{ position: 'relative' }}>
+              <img className="rounded-start w-100" src={image} alt={title} width="720" />
+              <img src={overlayUrl}
+                alt={`${title} ${platformName}`}
+                style={{
+                  position: 'absolute',
+                  bottom: '3%',
+                  right: '4%',
+                  maxWidth: '20%',
+                  maxHeight: '20%',
+                }}
+              />
+            </div>
+          </div>
 
-    <div className="col-lg-4 p-0 overflow-hidden shadow-lg order-lg-1">
-      <div className="lc-block"><img className="rounded-start w-100" src={image}
-       alt={title} width="720"></img></div>
-    </div>
+          <div className="col-lg-7 p-3 p-lg-5 pt-lg-3 order-lg-2">
+            <div className="lc-block mb-3">
+              <div editable="rich">
+                <h2 className="fw-bold display-4">{title}</h2>
+                <div className='w-100 d-inline-block'>
+                  <h3 className="fw-bold display-5 float-end ">{price}€</h3></div>
+              </div>
+            </div>
 
-    <div className="col-lg-7 p-3 p-lg-5 pt-lg-3 order-lg-2">
-      <div className="lc-block mb-3">
-        <div editable="rich">
-          <h2 className="fw-bold display-4">{title}<p></p><p></p></h2>
+            <div className="lc-block mb-3">
+              <div editable="rich">
+                <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world's most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+              </div>
+            </div>
+
+            <div className="d-grid gap-2 d-md-flex justify-content-md-between">
+              <p>Stock: {stock}</p>
+              <a className="btn btn-primary w-lg-100 px-4 me-md-2" href="#" role="button">Add to Cart</a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="lc-block mb-3">
-        <div editable="rich">
-          <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-        </div>
-      </div>
-
-      <div className="lc-block d-grid gap-2 d-md-flex justify-content-md-start">
-        <a className="btn btn-primary px-4 me-md-2" href="#" role="button">Click me, I'm a button</a>
-        <a className="btn btn-outline-secondary px-4" href="#" role="button">Click me, I'm a button</a>
-      </div>
-    </div>
-  </div>
-</div>
-
-    
     </>
   ) 
-
 }
 
 export default ProductDetails
--- src/pages/TestPageJanis/TestPageJanis.jsx
+++ src/pages/TestPageJanis/TestPageJanis.jsx
@@ -1,15 +1,18 @@
 import { useParams } from 'react-router-dom'
 import ProductDetails from '../../components/ProductDetails/ProductDetails.jsx'
 import data from '../../data/twelveProducts.js'
+import useApi from '../../hooks/useApi.js'
 
 
 function TestPageJanis() {
   const { id } = useParams()
+
+  const smth = useApi()
+
   const product = data.products.find(product => product._id === id);
 
   return (
     <>
-      <p>Product: {id}</p>
       <div className='productDetails>'>
         <ProductDetails product={product}/> 
       </div>
