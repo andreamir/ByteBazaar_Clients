fb3dbd418acfd6cee40a8c7d16d9fd62dd223bd1
nuinn <dilley.marc@gmail.com>
2024-03-11 09:23:52 +0100

    bug fixes and console.log cleanup

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,9 +1,12 @@
 import useApi from "../../hooks/useApi"
 import { useEffect } from 'react'
+import useToggle from '../../hooks/use-toggle.hook';
+import LoginModal from '../../components/LoginModal/LoginModal';
 import Profile from '../../components/Profile/Profile.jsx'
 import StyledBody from '../../components/Styled/Body.js'
 
 function Account() {
+  const [showLoginModal, toggleShowLoginModal] = useToggle(false);
   const { data, error, isLoading, getData } = useApi()
   const petition = {
     route: '/users/profile',
@@ -13,6 +16,12 @@ function Account() {
     getData(petition)
   }, [])
 
+  useEffect(() => {
+    if (typeof error === 'object' && error.msg === 'Unauthorised') {
+      toggleShowLoginModal(true)
+    }
+  }, [error])
+
   function reloadData() {
     getData(petition)
   }
@@ -21,7 +30,12 @@ function Account() {
     <>
       <StyledBody>
         {data && <Profile reload={reloadData} data={data}></Profile>}
-        {error && <p>{error.msg}</p>}
+        {/* {error && <p>{error.msg}</p>} */}
+        {showLoginModal && <LoginModal
+          handleDismiss={toggleShowLoginModal}
+          // toggleRegisterModal={toggleRegisterModal}
+          // toggleRecoveryModal={toggleRecoveryModal}
+        />}
         {isLoading && <p>Loading</p>}
       </StyledBody>
     </>
